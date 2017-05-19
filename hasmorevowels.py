b5d2dfce597c707c04f91068a6a15f2c849041f3
Allie Glotfelty <allie.glotfelty@gmail.com>
2017-05-19 11:02:42 -0700

    Added solution to hasmorevowels.py

---
+++ hasmorevowels.py
@@ -0,0 +1,49 @@
+"""Does word contain more vowels than non-vowels?
+
+If the word is over half vowels, it should return True:
+
+    >>> has_more_vowels("moose")
+    True
+
+If it's half vowels (or less), it's false:
+
+    >>> has_more_vowels("mice")
+    False
+
+    >>> has_more_vowels("graph")
+    False
+
+Don't consider "y" as a vowel:
+
+    >>> has_more_vowels("yay")
+    False
+
+Uppercase vowels are still vowels:
+
+    >>> has_more_vowels("Aal")
+    True
+"""
+
+
+def has_more_vowels(word):
+    """Does word contain more vowels than non-vowels?"""
+
+    vowel_count = 0
+    non_vowel_count = 0
+
+    for char in word:
+        if char in "AEIOUaeiou":
+            vowel_count += 1
+        else:
+            non_vowel_count += 1
+
+    if vowel_count > non_vowel_count:
+        return True
+
+    return False
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. HOORAY!\n"
