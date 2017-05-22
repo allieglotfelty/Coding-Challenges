c139573b0ea4c38c27564f7076c614fd6222fe3e
Allie Glotfelty <allie.glotfelty@gmail.com>
2017-05-22 13:53:02 -0400

    Added solution to leet.py

---
+++ leet.py
@@ -0,0 +1,41 @@
+"""Translate phrase to "leet-speak".
+
+For example::
+
+    >>> translate_leet("Hi Balloonicorn")
+    'Hi B@1100nic0rn'
+
+    >>> translate_leet("Hackbright is the Shizzle")
+    'H@ckbrigh7 i5 7h3 5hizz13'
+
+"""
+
+
+def translate_leet(phrase):
+    """Translates input into "leet-speak"."""
+
+    translator = {'a': '@',
+                  'o': '0',
+                  'e': '3',
+                  'l': '1',
+                  's': '5',
+                  't': '7'
+                  }
+
+    leet_phrase = ''
+
+    for letter in phrase:
+        if letter.lower() in translator:
+            leet_phrase += translator[letter.lower()]
+        else:
+            leet_phrase += letter
+
+    return leet_phrase
+
+
+
+if __name__ == '__main__':
+    import doctest
+
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. AE3S0M3!\n"
