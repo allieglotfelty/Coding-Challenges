d7c946bb14349ce60fbe969de3fd33ac23c8dcde
Allie Glotfelty <allie.glotfelty@gmail.com>
2017-06-13 11:32:35 -0700

    Added solution for cousin_solution.py

---
+++ cousin_solution.py
@@ -0,0 +1,137 @@
+"""Find 'cousin' nodes -- those nodes at the same level as a node.
+
+Consider the tree::
+
+            a
+        ---------
+       /    |    \
+      b     c     d
+     /-\   /-\   /-\
+    e  f  g  h  i  j
+        \     \
+        k     l
+
+Nodes `k` and `l` are at same level ("cousins", as we'll call them, but removed
+by several levels). Similarly `e`, `f`, `g`, `h`, `i`, and `j` are cousins, as are
+`b`, `c`, and `d`.
+
+Let's create this tree::
+
+    >>> a = Node("a")
+
+    >>> b = Node("b")
+    >>> c = Node("c")
+    >>> d = Node("d")
+    >>> b.set_parent(a)
+    >>> c.set_parent(a)
+    >>> d.set_parent(a)
+
+    >>> e = Node("e")
+    >>> f = Node("f")
+    >>> g = Node("g")
+    >>> h = Node("h")
+    >>> i = Node("i")
+    >>> j = Node("j")
+    >>> e.set_parent(b)
+    >>> f.set_parent(b)
+    >>> g.set_parent(c)
+    >>> h.set_parent(c)
+    >>> i.set_parent(d)
+    >>> j.set_parent(d)
+
+    >>> k = Node("k")
+    >>> l = Node("l")
+    >>> k.set_parent(f)
+    >>> l.set_parent(h)
+
+Let's find the cousins for b::
+
+    >>> b.cousins() == {c, d}
+    True
+
+    >>> c.cousins() == {b, d}
+    True
+
+    >>> e.cousins() == {f, g, h, i, j}
+    True
+
+    >>> k.cousins() == {l}
+    True
+
+The root node has no cousins::
+
+    >>> a.cousins() == set()
+    True
+"""
+
+
+class Node(object):
+    """Doubly-linked node in a tree.
+
+        >>> na = Node("na")
+        >>> nb1 = Node("nb1")
+        >>> nb2 = Node("nb2")
+
+        >>> nb1.set_parent(na)
+        >>> nb2.set_parent(na)
+
+        >>> na.children
+        [<Node nb1>, <Node nb2>]
+
+        >>> nb1.parent
+        <Node na>
+    """
+
+    parent = None
+
+    def __init__(self, data):
+        self.children = []
+        self.data = data
+
+    def __repr__(self):
+        return "<Node %s>" % self.data
+
+    def set_parent(self, parent):
+        """Set parent of this node.
+
+        Also sets the children of the parent to include this node.
+        """
+
+        self.parent = parent
+        parent.children.append(self)
+
+    def cousins(self):
+        """Find nodes on the same level as this node."""
+
+        current = self
+        cousin_depth = 0
+
+        while current.parent is not None:
+            cousin_depth += 1
+            current = current.parent
+
+        cousins = set()
+
+        def _cousins(node, curr_depth):
+            if node is None:
+                return
+
+            if curr_depth == cousin_depth:
+                cousins.add(node)
+                return
+
+            for child in node.children:
+                _cousins(child, curr_depth + 1)
+
+        _cousins(node=current, curr_depth=0)
+
+        cousins.remove(self)
+        return cousins
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED; GREAT JOB! ***\n"
